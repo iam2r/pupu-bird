2b8609ab264299a0e87f09fd241203be42c9775b
iam2r <bestwishtous@gmail.com>
2026-06-29 13:39:03 +0800

    隐私授权+保存相册

--- game.js
+++ game.js
@@ -14,6 +14,17 @@ ctx.scale(dpr, dpr);
 var lastTime = 0;
 game.init(canvas, ctx, {});
 
+// 隐私授权处理
+if (wx.onNeedPrivacyAuthorization) {
+  wx.onNeedPrivacyAuthorization(function(resolve) {
+    wx.showModal({
+      title: '隐私授权',
+      content: '噗噗鸟需要相册权限来保存纪念卡',
+      success: function(r) { resolve({ event: r.confirm ? 'agree' : 'disagree' }); }
+    });
+  });
+}
+
 wx.onTouchStart(function(e) { game.onTouch(e); });
 wx.onTouchMove(function(e) { game.onTouch(e); });
 wx.onTouchEnd(function(e) { game.onTouch(e); });
--- src/memorial.js
+++ src/memorial.js
@@ -202,24 +202,32 @@ function saveToAlbum() {
   memorialCanvas.toTempFilePath({
     success: function(res) {
       _shareImagePath = res.tempFilePath;
-      wx.saveImageToPhotosAlbum({
-        filePath: res.tempFilePath,
-        success: function() { wx.showToast({ title: '已保存到相册', icon: 'none' }); },
-        fail: function(err) {
-          console.log('saveImageToPhotosAlbum fail:', JSON.stringify(err));
-          if (err.errMsg && err.errMsg.indexOf('auth') >= 0) {
-            wx.showModal({
-              title: '需要相册权限',
-              content: '请允许噗噗鸟访问相册以保存纪念卡',
-              success: function(modalRes) {
-                if (modalRes.confirm) wx.openSetting({});
-              }
-            });
-          } else {
-            wx.showToast({ title: '保存失败，请重试', icon: 'none' });
+      if (wx.requirePrivacyAuthorize) {
+        wx.requirePrivacyAuthorize({
+          success: function() { doSave(res.tempFilePath); },
+          fail: function() { doSave(res.tempFilePath); }
+        });
+      } else {
+        doSave(res.tempFilePath);
+      }
+      function doSave(fp) {
+        wx.saveImageToPhotosAlbum({
+          filePath: fp,
+          success: function() { wx.showToast({ title: '已保存到相册', icon: 'none' }); },
+          fail: function(err) {
+            console.log('save fail:', JSON.stringify(err));
+            if (err.errMsg && err.errMsg.indexOf('auth') >= 0) {
+              wx.showModal({
+                title: '需要相册权限',
+                content: '请在设置中允许相册权限',
+                success: function(mr) { if (mr.confirm) wx.openSetting({}); }
+              });
+            } else {
+              wx.showToast({ title: '保存失败', icon: 'none' });
+            }
           }
-        }
-      });
+        });
+      }
     },
     fail: function() { wx.showToast({ title: '生成图片失败', icon: 'none' }); }
   });
